7b4ea275eb2b11f9c0a1b3c5999d84d7025b6e12
StanleyKioko <mwangestanley02@gmail.com>
2025-06-28 17:15:13 +0300

    Footer well adjusted

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -12,24 +12,20 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   const { isOpen } = useContext(SidebarContext);
 
   return (
-    <div className="flex min-h-screen bg-[#f5f6f5] overflow-hidden flex-col">
+    <div className="flex min-h-screen bg-[#f5f6f5] flex-col">
       <div className="flex flex-1 relative">
         <Sidebar />
         <div
-          className="flex-1 flex flex-col transition-all duration-300 ease-in-out main-content"
+          className="main-content"
           style={{ marginLeft: isOpen ? '280px' : '88px' }}
         >
           <Header />
-          <div className="flex-1 flex flex-col">
-            <main className="p-4 md:p-6 lg:p-8 flex-1 overflow-y-auto">
-              <div className="max-w-7xl mx-auto">
-                <div className="grid grid-cols-1 gap-6 animate-fade-in-up">
-                  {children}
-                </div>
-              </div>
-            </main>
-            <Footer />
-          </div>
+          <main className="flex-1 overflow-y-auto">
+            <div className="max-w-7xl mx-auto p-4 md:p-6 lg:p-8">
+              {children}
+            </div>
+          </main>
+          <Footer />
         </div>
       </div>
     </div>
